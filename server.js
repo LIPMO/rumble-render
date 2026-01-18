901f35a5b0f04d4aba97c16261183780dca9e715
Je suis bleu <93595424+LIPMO@users.noreply.github.com>
2026-01-18 12:32:43 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const WebSocket = require("ws");
+
+const PASSWORD = process.env.PASSWORD || "1234";
+
+const app = express();
+app.use(express.static("public"));
+
+const server = app.listen(process.env.PORT || 3000, () =>
+  console.log("Server running")
+);
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", ws => {
+  ws.on("message", msg => {
+    try {
+      const data = JSON.parse(msg.toString());
+      if (data.password !== PASSWORD) return;
+
+      wss.clients.forEach(c => {
+        if (c.readyState === WebSocket.OPEN) {
+          c.send(JSON.stringify(data));
+        }
+      });
+    } catch {}
+  });
+});
